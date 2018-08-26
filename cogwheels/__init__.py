69b9f934382e1d6a9e561ed7216f7078220c1b44
Andy Babic <ababic@rkh.co.uk>
2018-08-26 14:16:49 +0100

    Remove a trailing comma

--- cogwheels/__init__.py
+++ cogwheels/__init__.py
@@ -8,6 +8,6 @@ from .exceptions import ( # noqa
     DefaultValueFormatInvalid, DefaultValueNotImportable,
     OverrideValueError, OverrideValueTypeInvalid,
     OverrideValueFormatInvalid, OverrideValueNotImportable,
-    UnknownSettingNameError,
+    UnknownSettingNameError
 )
 from .helpers import BaseAppSettingsHelper, DeprecatedAppSetting # noqa
